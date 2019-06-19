a2d1e2eb4bd7bead90e0742fc8c2521160dfebdb
Denali Balser <denali.balser167@gmail.com>
2019-06-19 18:57:37 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,21 @@
-CREATE TABLE project (id INTEGER PRIMARY KEY,
-title TEXT, category TEXT, funding_goal INTEGER,
-start_date TEXT, end_date TEXT);
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
 
-CREATE TABLE user (id INTEGER PRIMARY KEY,
-name TEXT, age INTEGER);
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
-CREATE TABLE pledge (id INTEGER PRIMARY KEY,
-amount INTEGER, project_id INTEGER,
-user_id INTEGER);
+CREATE TABLE pledge (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  project_id INTEGER,
+  user_id INTEGER
+);
